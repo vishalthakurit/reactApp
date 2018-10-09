46f49d71ce8e0cf06afbb22b670b826d8f3a250d
Vishal Thakur <vshlrjpt80@gmail.com>
2018-10-10 01:02:45 +0530

    create form

--- src/Form.js
+++ src/Form.js
@@ -8,34 +8,67 @@ class Form extends Component {
             username : '',
             allUsers : [],
             radioGroup : {
-                phpradio: "true",
-                javaradio: "false",
-                rubyradio : "false"
-            }
+                phpradio: false,
+                javaradio: true,
+                rubyradio : false,
+                radioGroupClass : 'radio_section'
+            },
+            checkboxGroup : {
+                reacttech : true,
+                nodetech : false,
+                angulartech : false,
+                checkboxGroupClass : 'checkbox_section' 
+            },
+            selectedValue : 'express'
         }
         this.saveUsers = this.saveUsers.bind(this);
         this.radioHandler = this.radioHandler.bind(this);
+        this.checkboxHandler = this.checkboxHandler.bind(this);
+        this.selectHandler = this.selectHandler.bind(this);
     }
-
+    
     updateUserName(event){
-        // console.log(event.target.value);
         this.setState({
             username: event.target.value
         })
     }
 
     radioHandler(radio) {
-        console.log('radio');
-        console.log(radio);
+        let radioButton = this.state.radioGroup;
+        for (var key in radioButton) {
+            radioButton[key] = false;
+        }
+        radioButton[radio.target.value] = radio.target.checked;   
+         this.setState({
+             radioGroup: radioButton
+         })
+    }
+
+    checkboxHandler(check) {
+        console.log(check.target.value);
+        let checkboxCont = this.state.checkboxGroup;
+        checkboxCont[check.target.value] = check.target.checked;
+        this.setState({
+            checkboxGroup : checkboxCont
+        })
+    }
+
+    selectHandler(sel) {
+        //console.log(sel.target.value);
+        this.setState({
+            selectedValue : sel.target.value
+        })
     }
 
     saveUsers(ev) {
         let currentUser = this.state.allUsers;
-        currentUser.push(this.state.username);
-        this.setState({
-            allUsers : currentUser,
-            username : ""
-        })
+        if (this.state.username && this.state.username !== "") {
+            currentUser.push(this.state.username);
+            this.setState({
+                allUsers : currentUser,
+                username : ""
+            })
+        }
         //console.log(this.state.allUsers);
         ev.preventDefault();
     }
@@ -48,24 +81,50 @@ class Form extends Component {
                     <input type="text" name="username" value={this.state.username} onChange={this.updateUserName.bind(this)}/>
                 </label>
                 {/* <p>UserName is :{this.state.username}</p> */}
-                <div>
+                { /* RADIO SECTION */ }
+                <div className={this.state.radioGroup['radioGroupClass']}>
                     Developers Categories : 
                     <br/>
                     <label>
                         PHP :  
-                        <input type="radio" name="devcategory" value="php" checked={this.state.radioGroup['phpradio']} onChange={this.radioHandler}></input>
+                        <input type="radio" name="devcategory" value="phpradio" checked={this.state.radioGroup['phpradio']} onChange={this.radioHandler}></input>
                     </label>
                     <br />
                     <label>
                         JAVA :
-                        <input type="radio" name="devcategory" value="java" checked={this.state.radioGroup['javaradio']} onChange={this.radioHandler}></input>
+                        <input type="radio" name="devcategory" value="javaradio" checked={this.state.radioGroup['javaradio']} onChange={this.radioHandler}></input>
                     </label>
                     <br />
                     <label>
                         RUBY :
-                        <input type="radio" name="devcategory" value="ruby" checked={this.state.radioGroup['rubyradio']} onChange={this.radioHandler}></input>
+                        <input type="radio" name="devcategory" value="rubyradio" checked={this.state.radioGroup['rubyradio']} onChange={this.radioHandler} />
                     </label>
                 </div>
+                {/* CHECKBOX SECTION */}
+                <div className={this.state.radioGroup['checkboxGroupClass']}>
+                    Technology Categories : 
+                    <br/>
+                    <label>
+                        REACT :  
+                        <input type="checkbox" name="tech" value="reacttech" checked={this.state.checkboxGroup['reacttech']} onChange={this.checkboxHandler}></input>
+                    </label>
+                    <br />
+                    <label>
+                        NODE :
+                        <input type="checkbox" name="tech" value="nodetech" checked={this.state.checkboxGroup['nodetech']} onChange={this.checkboxHandler}></input>
+                    </label>
+                    <br />
+                    <label>
+                        ANGULAR :
+                        <input type="checkbox" name="tech" value="angulartech" checked={this.state.checkboxGroup['angulartech']} onChange={this.checkboxHandler} />
+                    </label>
+                </div>
+                {/* SELECT FIELD */}
+                <select value={this.state.selectedValue} onChange={this.selectHandler}> 
+                    <option value="ruby">Ruby</option>
+                    <option value="python">Python</option>
+                    <option value="express">Express</option>
+                </select>
                 <button onClick={this.saveUsers}>Save</button>
             </form>
         )
